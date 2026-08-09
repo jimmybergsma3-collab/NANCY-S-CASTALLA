6c02b325182a540b5490e3bc5c6c261123d44e57
Jimmy Bergsma <jimmybergsma3@gmail.com>
2026-08-09 17:13:41 +0200

    Add ingredients batch ten

---
+++ supabase/migrations/202608090023_product_ingredients_batch_10.sql
@@ -0,0 +1,163 @@
+-- Ingredients review batch 10 for active products.
+-- Checked on 2026-08-09 against manufacturer, wholesaler, or product-data sources.
+-- Additional information is intentionally cleared for this batch.
+
+-- Source: https://www.flyinggoosebrand.com/product/sweet-chilli-sauce/
+update products
+   set ingredients = 'Sugar syrup, chilli 22%, garlic, distilled vinegar, water, salt, modified tapioca starch, acidity regulator E330, acid E260, stabiliser E415 and preservative E202.',
+       directions = 'Ready to use as a dipping sauce, glaze, marinade or cooking sauce. Add to taste.',
+       conservation = 'Store in a cool, dry place away from direct sunlight. Refrigerate after opening and consume according to packaging.',
+       additional_info = ''
+ where id = 'NC-03090';
+
+-- Source: https://www.remia.com/en/products/remia-like-mayo-chipotle-tube-15x800ml
+update products
+   set ingredients = 'Rapeseed oil 60%, water, sugar, spirit vinegar, chilli pepper paste, salt, herbs, modified potato starch, chipotle powder, acidifier, spices, roasted onion, thickeners, natural flavouring, preservative, smoke flavourings and antioxidant.',
+       directions = 'Ready to use as a chipotle mayonnaise with fries, burgers, wraps, chicken dishes or snacks.',
+       conservation = 'Store in a cool, dry place. Refrigerate after opening if required by packaging.',
+       additional_info = ''
+ where id = 'NC-03097';
+
+-- Source: https://www.foodworld-xl.com/p/remia-legendary-real-tasty-mayonnaise-spicy-sriracha-15x-800ml
+update products
+   set ingredients = 'Rapeseed oil 68%, water, vinegar, free-range EGG yolk, sugar, chilli pepper, salt, garlic, MUSTARD, modified corn starch, lemon puree, lactic acid, natural flavouring, paprika powder, guar gum, paprika extract, potassium sorbate and antioxidant E385. Contains EGG and MUSTARD.',
+       directions = 'Ready to use as spicy sriracha mayonnaise with fries, burgers, chicken, wraps or loaded fries.',
+       conservation = 'Store in a cool, dry place. Refrigerate after opening if required by packaging.',
+       additional_info = ''
+ where id = 'NC-03098';
+
+-- Source: https://www.bestwaywholesale.co.uk/product/273284-1
+update products
+   set ingredients = 'Sunflower oil 50%, water, spirit vinegar, tomato puree, EGG yolk, sugar, garlic, flavourings, modified starch, salt, MUSTARD, spices, herb, onion, xanthan gum, potassium sorbate, smoke flavourings and antioxidant. Contains EGG and MUSTARD.',
+       directions = 'Ready to use as a smoky bacon-style mayonnaise sauce with fries, burgers, chicken or sandwiches.',
+       conservation = 'Store in a cool, dry place. Refrigerate after opening if required by packaging.',
+       additional_info = ''
+ where id = 'NC-03113';
+
+-- Source: https://www.heinz.co.uk/sauces
+update products
+   set ingredients = 'Barbecue style sauce with tomato, sugar, vinegar, molasses or syrup, salt, spices, smoke flavouring and thickeners. Check packaging for full ingredients and allergens.',
+       directions = 'Ready to use as a barbecue sauce, dip, glaze or marinade. Add to taste.',
+       conservation = 'Store in a cool, dry place. Refrigerate after opening and consume according to packaging.',
+       additional_info = ''
+ where id = 'NC-03116';
+
+-- Source: https://www.remia.com/en/products
+update products
+   set ingredients = 'Mayonnaise style sauce with vegetable oil, water, vinegar, EGG yolk, truffle flavouring, mustard, salt, sugar, starch, thickeners and preservatives. Contains EGG and may contain MUSTARD.',
+       directions = 'Ready to use with fries, burgers, sandwiches, carpaccio, salads or snacks.',
+       conservation = 'Store in a cool, dry place. Refrigerate after opening if required by packaging.',
+       additional_info = ''
+ where id = 'NC-03117';
+
+-- Source: https://www.heinz.co.uk/sauces
+update products
+   set ingredients = 'Creamy bacon-style sauce with vegetable oil, water, vinegar, egg or dairy ingredients depending on label, smoke flavouring, spices, salt, sugar and thickeners. Check packaging for full allergens.',
+       directions = 'Ready to use as a creamy bacon-style sauce with fries, burgers, sandwiches, chicken or snacks.',
+       conservation = 'Store in a cool, dry place. Refrigerate after opening and consume according to packaging.',
+       additional_info = ''
+ where id = 'NC-03118';
+
+-- Source: https://untappd.com/w/brasserie-jupiler/5460/beer
+update products
+   set ingredients = 'Water, barley malt, maize, hops and yeast. Contains BARLEY/GLUTEN.',
+       directions = 'Serve chilled. Alcoholic drink; consume responsibly.',
+       conservation = 'Store cool, dry and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03168';
+
+-- Source: https://www.guinness-storehouse.com/en/whats-hoppening/most-popular-questions-about-guinness
+update products
+   set ingredients = 'Water, malted barley, roasted barley, hops and yeast. Contains BARLEY/GLUTEN.',
+       directions = 'Serve chilled. Alcoholic drink; consume responsibly.',
+       conservation = 'Store cool, dry and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03169';
+
+-- Source: https://www.britishstoreonline.com/shop/product/john-smiths-extra-smooth-ale-4-x-440ml-cans
+update products
+   set ingredients = 'Water, malted BARLEY, glucose syrup, BARLEY, hops, hop extract and nitrogen. Contains BARLEY/GLUTEN.',
+       directions = 'Serve chilled. Alcoholic drink; consume responsibly.',
+       conservation = 'Store ambient, cool and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03170';
+
+-- Source: https://en.wikipedia.org/wiki/Stella_Artois
+update products
+   set ingredients = 'Water, malted barley, maize, hops and yeast. Contains BARLEY/GLUTEN.',
+       directions = 'Serve chilled. Alcoholic drink; consume responsibly.',
+       conservation = 'Store cool, dry and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03171';
+
+-- Source: https://www.tennents.com/us/our-beers/tennents-lager
+update products
+   set ingredients = 'Water, Scottish malted BARLEY, hops and yeast. Contains BARLEY/GLUTEN.',
+       directions = 'Serve chilled. Alcoholic drink; consume responsibly.',
+       conservation = 'Store cool, dry and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03172';
+
+-- Source: https://www.tennents.com/us/our-beers/tennents-lager
+update products
+   set ingredients = 'Water, Scottish malted BARLEY, hops and yeast. Contains BARLEY/GLUTEN.',
+       directions = 'Serve chilled. Alcoholic drink; consume responsibly.',
+       conservation = 'Store cool, dry and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03173';
+
+-- Source: https://www.tesco.com/shop/en-GB/products/299537875
+update products
+   set ingredients = 'Apple cider with blackcurrant and blackberry flavourings, sugar and sweeteners. Contains SULPHITES.',
+       directions = 'Serve chilled or over ice. Alcoholic drink; consume responsibly.',
+       conservation = 'Store ambient, cool and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03174';
+
+-- Source: https://www.coronausa.com/pages/corona-extra
+update products
+   set ingredients = 'Water, barley malt, non-malted cereals and hops. Contains BARLEY/GLUTEN.',
+       directions = 'Serve chilled. Alcoholic drink; consume responsibly.',
+       conservation = 'Store cool, dry and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03176';
+
+-- Source: https://www.birraperoni.it/en/our-beers/our-ingredients/
+update products
+   set ingredients = 'Water, barley malt, yeast, hops and Nostrano maize. Contains BARLEY/GLUTEN.',
+       directions = 'Serve chilled. Alcoholic drink; consume responsibly.',
+       conservation = 'Store cool, dry and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03177';
+
+-- Source: https://sodexo-cy.com/forces-store/drinks/beer-cider/rekorderlig-strawberry-lime-cider-500ml
+update products
+   set ingredients = 'Carbonated water, pear and apple wines with strawberry-lime flavour, sugar, flavours, citric acid, sodium citrate, colours, potassium sorbate and sulphur dioxide. Contains SULPHITES.',
+       directions = 'Serve chilled over ice. Alcoholic drink; consume responsibly.',
+       conservation = 'Store ambient, cool and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03178';
+
+-- Source: https://littlefinland.de/en/beer-cider-long-drink/640-kopparberg-premium-wildberries-cider-45-033l.html
+update products
+   set ingredients = 'Water, carbon dioxide, cider, apple/berry juice, sugar, flavours, citric acid, preservative E202 and potassium disulphite. Contains SULPHITES.',
+       directions = 'Serve chilled or over ice. Alcoholic drink; consume responsibly.',
+       conservation = 'Store ambient, cool and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03180';
+
+-- Source: https://kopparberg.co.uk/faqs/
+update products
+   set ingredients = 'Carbonated water, fermented pear or apple juice, sugar, flavouring, potassium sorbate E202 and sulphite E224. Contains SULPHITES.',
+       directions = 'Serve chilled or over ice. Alcoholic drink; consume responsibly.',
+       conservation = 'Store ambient, cool and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03181';
+
+-- Source: https://kopparberg.us/our-products/kopparberg-strawberry-lime/
+update products
+   set ingredients = 'Hard apple cider, carbonated water, sugar, juice including elderberry, strawberry and lime, flavouring, citric acid, potassium sorbate and sulphites. Contains SULPHITES.',
+       directions = 'Serve chilled or over ice. Alcoholic drink; consume responsibly.',
+       conservation = 'Store ambient, cool and away from direct sunlight.',
+       additional_info = ''
+ where id = 'NC-03182';
